3bdeca1b0ca2ea59994c0a686874a7cea55eb4d1
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-04-05 17:40:24 +0000

    Update docs after building Travis build 14 of vstadnytskyi/github-practice-template
    The docs were built from the branch 'master' against the commit
    de13eb1fe0823079811bce7ea9cc0318db401ecb.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/vstadnytskyi/github-practice-template/jobs/496191430.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs/build/html/

--- _sources/lessons.rst.txt
+++ _sources/lessons.rst.txt
@@ -2,6 +2,6 @@
 Lessons
 =======
 
-Lesson 1
+Lesson 5
 ----------------------------
 In this lesson, we will fix the function that does not work correctly and pytest returns "failed" flag.
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post13+gbada8c9',
+    VERSION: '0.post14+gde13eb1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
