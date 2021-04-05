e799f25f8e8d0600dbec8328fdcdf5f3499d7ef0
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2021-04-05 13:49:51 -0400

    multiply func and test

--- github_practice_template/numerical/algorithms.py
+++ github_practice_template/numerical/algorithms.py
@@ -19,3 +19,22 @@ def sum(num1,num2):
     >>> num = sum(1,3)
     """
     return num1 + num2 + 1
+
+  # def multiply(num1,num2):
+  #     """
+  #     calculates the product of two numbers
+  #
+  #     Parameters
+  #     ----------
+  #     num1 (float)
+  #     num2 (float)
+  #
+  #     Returns
+  #     -------
+  #     num (float)
+  #
+  #     Examples
+  #     --------
+  #     >>> num = multiply(1,3)
+  #     """
+  #     return num1*num2
--- github_practice_template/tests/test_numerical.py
+++ github_practice_template/tests/test_numerical.py
@@ -5,3 +5,7 @@ class NumericalTest(unittest.TestCase):
     def test_sum(self):
         from ..numerical import algorithms
         self.assertEqual(algorithms.sum(1,2), 3)
+
+  # def test_multiply(self):
+  #     from ..numerical import algorithms
+  #     self.assertEqual(algorithms.multiply(3,4), 12)
