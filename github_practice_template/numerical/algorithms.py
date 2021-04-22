d743434964853799f03dc127c136b1d81a694a15
Sakshi Tomar <sak6861g@gmail.com>
2021-04-22 17:42:55 -0400

    fixed tests and sum func

--- github_practice_template/numerical/algorithms.py
+++ github_practice_template/numerical/algorithms.py
@@ -20,21 +20,21 @@ def sum(num1,num2):
     """
     return num1 + num2
 
-   def multiply(num1,num2):
-       """
-       calculates the product of two numbers
-  
-       Parameters
-       ----------
-       num1 (float)
-       num2 (float)
-  
-       Returns
-       -------
-       num (float)
-  
-       Examples
-       --------
-       >>> num = multiply(1,3)
-       """
-       return num1*num2
+def multiply(num1,num2):
+   """
+   calculates the product of two numbers
+
+   Parameters
+   ----------
+   num1 (float)
+   num2 (float)
+
+   Returns
+   -------
+   num (float)
+
+   Examples
+   --------
+   >>> num = multiply(1,3)
+   """
+   return num1*num2
